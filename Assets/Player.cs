9996d11e9d66dce984e2c5199abfa0f9c076ec9d
Alex Smith <as61@iu.edu>
2020-10-22 12:10:31 -0400

    player can move, bounce off of and pass through platforms

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
+   [SerializeField] public float moveSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+         {
+             transform.position = transform.position += transform.right * -moveSpeed * Time.deltaTime;
+             GetComponent<SpriteRenderer>().flipX = true;
+         }
+ 
+        if (Input.GetKey(KeyCode.D))
+         { 
+             transform.position = transform.position += transform.right * moveSpeed * Time.deltaTime;
+             GetComponent<SpriteRenderer>().flipX = false;
+         }
+
     }
 }
