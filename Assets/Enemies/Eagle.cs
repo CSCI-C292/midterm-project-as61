eee26bbb922e87ad9fc5678e08119a78ef816e13
Alex Smith <as61@iu.edu>
2020-10-31 14:01:33 -0400

    player hits enemy and dies (Everything priot to UI introduction)

--- Assets/Enemies/Eagle.cs
+++ Assets/Enemies/Eagle.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class Eagle : MonoBehaviour
 {
+    [SerializeField] float bounceFactor;
+    GameObject player;
+   
+
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.relativeVelocity.y <= 0f){}
+
+        if (collision.relativeVelocity.y <= 0f)
+        {
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+            if (rb != null){
+                Vector2 velocity = rb.velocity;
+                velocity.y = bounceFactor;
+                rb.velocity = velocity;
+            }
+            Destroy(gameObject);
+
+            
+        }
+      //  Destroy(Collision2D.otherCollider);
 
     }
-    // Start is called before the first frame update
+
     void Start()
     {
         
--- Assets/player/PlayerMovement.cs
+++ Assets/player/PlayerMovement.cs
@@ -8,8 +8,13 @@ public class PlayerMovement : MonoBehaviour
    [SerializeField] public float moveSpeed = 10f;
    float _xMin;
    float _xMax;
+   Animator anim;
 
     // Start is called before the first frame update
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
     void Start()
     {
         _xMin = UnityEngine.Camera.main.ViewportToWorldPoint(new Vector3(0,0,0)).x;
@@ -45,10 +50,20 @@ public class PlayerMovement : MonoBehaviour
          if (transform.position.x > _xMax){
             SetTransformX(_xMin);
          }
+    }
 
-             
-        
-         
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.collider.tag == "Eagle") && (collision.relativeVelocity.y <= 0f)){
+            anim.SetTrigger("HitEagle");
+            Destroy(GetComponent<BoxCollider2D>());
+
+        }
+        if ((collision.collider.tag == "Eagle") && (collision.relativeVelocity.y >= 0f)){
+            anim.SetTrigger("OnBounce");
+            
+
+        }
 
     }
 
