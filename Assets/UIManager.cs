67fe2ce88e4646604ae1a438f6ecfdb5ab0192e9
Alex Smith <as61@iu.edu>
2020-11-03 14:50:29 -0500

    UI introduction

---
+++ Assets/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    public Button button01;
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        this.button01.onClick.AddListener(playClicked);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void playClicked()
+    {
+        SceneManager.LoadScene("Midterm Project");
+    }
+}
